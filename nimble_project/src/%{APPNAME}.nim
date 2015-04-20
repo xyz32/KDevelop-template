eb14f6c97fa943a44057397616191d3d52fe6fe3
Radu <radu32_xyz@yahoo.com>
2015-04-20 09:43:54 -0400

    cleanup license

--- nimble_project/src/%{APPNAME}.nim
+++ nimble_project/src/%{APPNAME}.nim
@@ -1,3 +1,13 @@
+{% load kdev_filters %}
+{% block license_header %}
+{% if license %}
+#
+
+{{ license|lines_prepend:"# " }}
+#
+{% endif %}
+{% endblock license_header %}
+
 proc hello() =
   echo "hello world"
   
