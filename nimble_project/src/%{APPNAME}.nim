1e4bc7cee3a718d9dc55246dd862df389432d03d
Radu <radu32_xyz@yahoo.com>
2015-04-20 09:44:53 -0400

    Remove license as it doesn't work

--- nimble_project/src/%{APPNAME}.nim
+++ nimble_project/src/%{APPNAME}.nim
@@ -1,12 +1,3 @@
-{% load kdev_filters %}
-{% block license_header %}
-{% if license %}
-#
-
-{{ license|lines_prepend:"# " }}
-#
-{% endif %}
-{% endblock license_header %}
 
 proc hello() =
   echo "hello world"
