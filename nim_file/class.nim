7595cb7341065b704b9557e46c76a698f0a624b8
Radu <radu32_xyz@yahoo.com>
2015-04-20 09:15:56 -0400

    created a basic nim object template

---
+++ nim_file/class.nim
@@ -0,0 +1,14 @@
+{% load kdev_filters %}
+{% block license_header %}
+{% if license %}
+#
+
+{{ license|lines_prepend:"# " }}
+#
+{% endif %}
+{% endblock license_header %}
+
+proc hello() =
+  echo "hello world"
+  
+hello()
